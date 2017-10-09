feef4f13ab10ee14a026c574ef0b398d95e8ea14
deepak bhoria <dpk22dev@gmail.com>
2017-10-10 00:29:15 +0530

    youtube broadcast, stream, binding saved to mongo vuora/broadcast collection

--- models/seminarData.js
+++ models/seminarData.js
@@ -1,11 +1,42 @@
+const mongo = require('../lib/mongo');
+const config = require('config');
+
 var dummyData = {
-    "snippet":{
-        "title":"test2"
+    "userID" : "123456",
+    "broadcast" : {
+        "part": "snippet,status,contentDetails",
+        "resource": {
+            "snippet": {
+                "title": "7pm event 21",
+                "description" : "broadcast is about...",
+                "scheduledStartTime": "2017-10-10T18:40:00.000Z",
+                "scheduledEndTime": "2017-10-10T21:00:00.000Z",
+            },
+            "status": {
+                "privacyStatus": "private",
+            },
+            "contentDetails": {
+                "monitorStream": {
+                    "enableMonitorStream": true,
+                }
+            }
+        }
     },
-    "cdn":
-    {
-        "format":"1080p",
-        "ingestionType":"dash"
+    "stream" : {
+        "part": "snippet, status, cdn",
+        "resource": {
+            "snippet": {
+                "title": "s 360 p2",
+                "desc": "for broadcast id"
+            },
+            "cdn": {
+                "format": "360p",
+                "ingestionType": "rtmp"
+            }
+        }
+    },
+    "binding" : {
+        "part": "id, snippet,status,contentDetails",
     }
 }
 /*
@@ -22,3 +53,34 @@ var dummyData = {
 
 
 exports.seminarDummyData = dummyData;
+
+exports.convertModel2UserData = function ( data ) {
+
+}
+
+
+exports.convertUser2ModelData = function ( data ) {
+
+}
+
+exports.insertSeminar = function ( data ) {
+    var broadcastCol = config.get("mongodb.broadcastCol");
+    var mongoDB = mongo.getInstance();
+    var collection = mongoDB.collection( broadcastCol );
+
+    var promise = collection.insertOne(data);
+    return promise;
+    
+}
+
+exports.updateSeminar = function () {
+    
+}
+
+exports.deleteSeminar =function () {
+    
+}
+
+exports.getSeminar = function () {
+    
+}
--- routes/seminar.js
+++ routes/seminar.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var youtubeApi = require('../lib/youtubeApi');
+const seminarModel= require('../models/seminarData');
 
 /* GET users listing. */
 router.get('/create', function(req, res, next) {
     var seminarData = require( '../models/seminarData' ).seminarDummyData;
+    //seminarModel.insertSeminar( seminarData );
+
+
     youtubeApi.createBroadcast( seminarData, function ( err, data ) {
-        res.send('youtube respond with a resource');
+        if( err ){
+            res.send('Error while integraing with youtube');
+        }
+        seminarModel.insertSeminar( data ).then( function ( ok ) {
+            res.send('inserted seminar in mongo');
+        }, function ( err ) {
+            res.send('error while inserting in mongo!');
+        });
+
     });
+
 });
 
 module.exports = router;
