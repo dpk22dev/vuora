94201445aa41dff629c336994e62bec91dbd9c36
Vinay Sahu <vinay.sahu@timesinternet.in>
2017-10-13 00:24:48 +0530

    seminar apis

--- routes/timeline.js
+++ routes/timeline.js
@@ -6,15 +6,65 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser');
-var calender = require('./../lib/calender');
+var calender = require('./../lib/seminar');
 var jsonParser = bodyParser.json({type: 'application/json'});
 
-router.post('/addevent', jsonParser, function (req, res) {
+router.post('/seminar/request', jsonParser, function (req, res) {
     var body = req.body;
-    calender.addEvent(body.from, body.to, body.type, body.description, body.requestor, body.requestee, function (err, result) {
-        res.stausCode = 202;
-        res.send();
+    calender.requestSeminar(body, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result)
+        }
     });
 });
 
+router.post('/seminar/accept', jsonParser, function (req, res) {
+    var body = req.body;
+    calender.acceptSeminar(body, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result)
+        }
+    });
+});
+
+router.post('/seminar/create', jsonParser, function (req, res) {
+    var body = req.body;
+    calender.createSeminar(body, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result)
+        }
+    });
+});
+
+router.delete('/seminar/:mid', function (req, res) {
+    var data = {
+        user: req.query.user,
+        tag: req.query.tag,
+        mid: req.params.mid
+    };
+    calender.deleteSeminar(data, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result)
+        }
+    })
+});
+
+router.post('/seminar/search', jsonParser, function (req, res) {
+    var body = req.body;
+    calender.searchSeminar(body, function (err, result) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result)
+        }
+    })
+});
 module.exports = router;
