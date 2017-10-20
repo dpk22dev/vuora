ede47e759611d38ccdfcdc04c35d1d2beaea06cd
deepak bhoria <dpk22dev@gmail.com>
2017-10-20 14:29:54 +0530

    mediastream recorder integrated with basic functionality

--- routes/f2f.js
+++ routes/f2f.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var path = require('path');
 var f2fData = require('../models/f2fData');
 
+var multer  = require('multer')
+var upload = multer({ dest: 'videos/' })
+/*
+
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json({type: 'application/json'});
+*/
 
 /*
  working with http://localhost:3000/f2f/alpha/?userId=1
@@ -16,6 +23,20 @@ router.get('/:f2fId', function(req, res, next) {
     } else {
         res.send('Access denied');
     }
+});
+
+//todo this should go in its own service
+router.post( '/saveBlob', upload.array(), function ( req, res, next ) {
+
+    req['video-filename'];
+    req['video-blob'];
+    var stream = fs.createWriteStream("my_file.txt");
+    stream.once('open', function(fd) {
+        stream.write("My first row\n");
+        stream.write("My second row\n");
+        stream.end();
+    });
+
 });
 
 module.exports = router;
