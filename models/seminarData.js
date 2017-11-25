a59275d1aae87ebc005a2043f5381545b4475c91
deepak bhoria <dpk22dev@gmail.com>
2017-11-25 14:35:28 +0530

    added appjs, seminarid instead of broadcast id

--- models/seminarData.js
+++ models/seminarData.js
@@ -262,7 +262,7 @@ exports.createStreamStatusData =function ( data ){
         "userID": data.userId,
         "stream": {
             "part": "status, id",
-            "id": data.broadcast.id
+            "id": data.stream.id
         }
     }
     return obj;
